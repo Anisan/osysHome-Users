67c4ec14a8dd5e5d0d8bdeb1c7869930e82d418f
Eraser <eraser1981@gmail.com>
2025-03-12 20:04:21 +0300

    add Guest add generate apikey

--- forms/UserForm.py
+++ forms/UserForm.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired
 class UserForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     image = StringField('Image')
-    role = SelectField('Role',choices=(("user","User"), ("editor","Editor"), ("admin","Administrator")), validators=[DataRequired()])
+    role = SelectField('Role',choices=(("user","User"),("guest","Guest"), ("editor","Editor"), ("admin","Administrator")), validators=[DataRequired()])
     home_page = StringField('Home page')
     apikey = StringField('API key')
     submit = SubmitField('Submit')
