ac89185924e008a32bc424c1f583ae9392e176b5
Eraser <eraser1981@gmail.com>
2025-12-21 12:59:00 +0300

    add select timezone

--- forms/UserForm.py
+++ forms/UserForm.py
@@ -1,6 +1,13 @@
 from flask_wtf import  FlaskForm
 from wtforms import StringField, SubmitField, SelectField 
 from wtforms.validators import DataRequired
+from zoneinfo import available_timezones
+
+
+def get_timezone_choices():
+    """Возвращает отсортированный список часовых поясов для выпадающего списка"""
+    timezones = sorted(available_timezones())
+    return [(tz, tz) for tz in timezones]
 
 
 class UserForm(FlaskForm):
@@ -9,5 +16,5 @@ class UserForm(FlaskForm):
     role = SelectField('Role',choices=(("user","User"),("guest","Guest"), ("editor","Editor"), ("admin","Administrator")), validators=[DataRequired()])
     home_page = StringField('Home page')
     apikey = StringField('API key')
-    timezone = StringField('Timezone')
+    timezone = SelectField('Timezone', choices=get_timezone_choices())
     submit = SubmitField('Submit')
